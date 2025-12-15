90be5e1db35f4d801c4e158882392577d215a5f7
Anastasiya Ivaneeva <anastasiya.ivaneeva@ladadidgit.io>
2025-12-15 21:17:43 +0700

    Выполнено повторение

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -3,17 +3,16 @@ package manager;
 import model.GroupData;
 import org.openqa.selenium.By;
 
-public class GroupHelper {
-    private ApplicationManager manager;
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(ApplicationManager manager){
+        super(manager);
 
-        this.manager = manager;
     }
 
     public void openGroupsPage() {
         if (!manager.isElementPresent(By.name("new"))) {
-            manager.driver.findElement(By.linkText("groups")).click();
+            click(By.linkText("groups"));
         }
     }
 
@@ -24,21 +23,62 @@ public class GroupHelper {
 
     public void createGroup(GroupData group) {
         openGroupsPage();
-        manager.driver.findElement(By.name("new")).click();
-        manager.driver.findElement(By.name("group_name")).click();
-        manager.driver.findElement(By.name("group_name")).sendKeys(group.name());
-        manager.driver.findElement(By.name("group_header")).click();
-        manager.driver.findElement(By.name("group_header")).sendKeys(group.header());
-        manager.driver.findElement(By.name("group_footer")).click();
-        manager.driver.findElement(By.name("group_footer")).sendKeys(group.footer());
-        manager.driver.findElement(By.name("submit")).click();
-        manager.driver.findElement(By.linkText("groups")).click();
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupsPage();
     }
 
     public  void removeGroup() {
         openGroupsPage();
-        manager.driver.findElement(By.name("selected[]")).click();
-        manager.driver.findElement(By.name("delete")).click();
-        manager.driver.findElement(By.linkText("groups")).click();
+        selectGroup();
+        removeSelectedGroup();
+        returnToGroupsPage();
     }
+
+    public void modifyGroup(GroupData modifiedGroup) {
+        openGroupsPage();
+        selectGroup();
+        initGroupModification();
+        fillGroupForm(modifiedGroup);
+        submitGroupModification();
+        returnToGroupsPage();
+    }
+
+    private void initGroupCreation() {
+        click(By.name("new"));
+    }
+
+
+    private void removeSelectedGroup() {
+        click(By.name("delete"));
+    }
+
+
+    private void submitGroupModification() {
+        click(By.name("update"));
+    }
+    private void returnToGroupsPage() {
+        click(By.linkText("groups"));
+
+    }
+
+    private void fillGroupForm(GroupData group) {
+        type(By.name("group_name"), group.name());
+        type(By.name("group_header"), group.header());
+        type(By.name("group_footer"), group.footer());
+    }
+
+
+    private void initGroupModification() {
+        click(By.name("edit"));
+    }
+
+    private void selectGroup() {
+        click(By.name("selected[]"));
+    }
+    private void submitGroupCreation() {
+        click(By.name("submit"));
+    }
+
 }
--- addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -1,4 +1,14 @@
 package tests;
 
-public class GroupModificationTests {
+import model.GroupData;
+import org.junit.jupiter.api.Test;
+
+public class GroupModificationTests extends TestBase {
+    @Test
+    void canModifyGroup() {
+        if (!app.groups().isGroupPresent()) {
+            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        }
+        app.groups().modifyGroup(new GroupData().withName("modified name"));
+    }
 }
