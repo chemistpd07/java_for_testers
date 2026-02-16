aaf23e153d55699c4845a3abcb0a317c217c87f1
Anastasiya Ivaneeva <anastasiya.ivaneeva@ladadidgit.io>
2026-02-16 10:37:21 +0700

    Добавлены параметризованные тесты для создания групп и контактов

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,17 +1,33 @@
 package tests;
 
 import model.ContactData;
+import model.GroupData;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void CanCreateContact(){
-        app.contacts().createContact(new ContactData(
-                "Kovrov",
-                "Sasha",
-                "Filatov street",
-                "email.ru",
-                "777777"));
+
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>();
+        for (int i = 0; i < 5; i++) {
+            result.add(new ContactData(
+                    randomString(i *10),
+                    randomString(i *10),
+                    randomString(i *10),
+                    randomString(i *10),
+                    randomString(i * 10)
+                    ));
+        }
+        return result;
+    }
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void canCreateMultipleContact(ContactData contact){
+        app.contacts().createContact(contact);
     }
 }
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -16,16 +16,12 @@ public class TestBase {
         }
 
     }
-    public String randomString(int n) {
+    public static String randomString(int n) {
         var rnd = new Random();
         var result = "";
         for (int i = 0; i < n; i++) {
             result = result + (char)('a' + rnd.nextInt(26));
         }
-        if (n < 20) {
-            result = result + '\'';
-        }
         return result;
-
     }
 }
